4f3787f2e036d1f26030baede59de21e9a5f7085
Andreas Stoll <andreasstoll@gmx.ch>
2022-09-12 09:12:42 +0200

    Verbesserrt

---
+++ einstieg_dict.py
@@ -0,0 +1,21 @@
+#Primitiv
+ahmet = 5
+beat = 4
+claudia = 5
+daniela = 3.5
+
+#print(ahmet)
+#print(beat)
+
+#Mit Listen
+#noten = [5,4,5,3.5]
+#namen = ["Ahmet", "Beat", "Claudia", "Daniela"]
+#for index in range(len(noten)):
+#    print(namen[index], noten[index])
+
+#Mit Dictionary
+dictionary = {"Ahmet": 5, "Beat": 4, "Claudia": 4.5, "Daniela": 3.5}
+#for i in dictionary.values():
+#    print(i)
+
+print(dictionary["Beat"])
